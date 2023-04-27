122686e1ae94955973a67cc6f8f590feed584d1f
Jakub Hojda <jakub.hojda@microsoft.wsei.edu.pl>
2023-04-27 13:08:57 +0200

    Lot of stuff

--- MultiSet/MultiSet.cs
+++ MultiSet/MultiSet.cs
@@ -150,7 +150,35 @@ namespace MultiSet
             dict = tempMultiSet.dict;
             return this;
         }
-        
+        public bool IsEmpty { get => Count == 0; }
+        public int this[T item] 
+        { 
+            get
+            {
+                if (!Contains(item)) return 0;
+                return dict[item];
+            }
+        }
+        public IReadOnlySet<T> AsSet()
+        {
+            var newSet = new HashSet<T>();
+            
+            foreach (var item in dict)
+                newSet.Add(item.Key);
+
+            return newSet;
+        }
+        public IReadOnlyDictionary<T, int> AsDictionary()
+        {
+            var newDict = new Dictionary<T, int>();
+
+            foreach (var item in dict)
+                newDict.Add(item.Key, item.Value);
+
+            return newDict;
+        }
+
+
         #endregion
 
         #region ToOtherType
@@ -170,6 +198,206 @@ namespace MultiSet
 
             return result;
         }
+
+        #endregion
+
+        #region Subset Superset Equals
+
+        public IEqualityComparer<T> Comparer => throw new NotImplementedException();
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            if (other is null) throw new ArgumentNullException();
+            
+            List<T> otherCopy = new();
+            foreach (var item in other)
+            {
+                otherCopy.Add(item);
+            }
+            List<T> msetCopy = new();
+            foreach (var item in dict)
+            {
+                for (int i = 0; i < item.Value; i++)
+                { 
+                    msetCopy.Add(item.Key); 
+                }
+            }
+
+            foreach (var item in msetCopy)
+            {
+                if (!otherCopy.Contains(item)) return false;
+                otherCopy.Remove(item);
+            }
+            return true;
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            if (other is null) throw new ArgumentNullException();
+
+            List<T> otherCopy = new();
+            foreach (var item in other)
+            {
+                otherCopy.Add(item);
+            }
+            List<T> msetCopy = new();
+            foreach (var item in dict)
+            {
+                for (int i = 0; i < item.Value; i++)
+                {
+                    msetCopy.Add(item.Key);
+                }
+            }
+
+            foreach (var item in msetCopy)
+            {
+                if (!otherCopy.Contains(item)) return false;
+                otherCopy.Remove(item);
+            }
+            return otherCopy.Count != 0;
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            if (other is null) throw new ArgumentNullException();
+
+            List<T> otherCopy = new();
+            foreach (var item in other)
+            {
+                otherCopy.Add(item);
+            }
+            List<T> msetCopy = new();
+            foreach (var item in dict)
+            {
+                for (int i = 0; i < item.Value; i++)
+                {
+                    msetCopy.Add(item.Key);
+                }
+            }
+
+            foreach (var item in otherCopy)
+            {
+                if (!msetCopy.Contains(item)) return false;
+                msetCopy.Remove(item);
+            }
+            return true;
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            if (other is null) throw new ArgumentNullException();
+
+            List<T> otherCopy = new();
+            foreach (var item in other)
+            {
+                otherCopy.Add(item);
+            }
+            List<T> msetCopy = new();
+            foreach (var item in dict)
+            {
+                for (int i = 0; i < item.Value; i++)
+                {
+                    msetCopy.Add(item.Key);
+                }
+            }
+
+            foreach (var item in otherCopy)
+            {
+                if (!msetCopy.Contains(item)) return false;
+                msetCopy.Remove(item);
+            }
+            return msetCopy.Count != 0;
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            if (other is null) throw new ArgumentNullException();
+            foreach (var dictEl in dict)
+            {
+                foreach (var otherEl in other)
+                {
+                    if (dictEl.Equals(otherEl)) return true;
+                }
+            }
+            return false;
+        }
+
+        public bool MultiSetEquals(IEnumerable<T> other)
+        {
+            if (other is null) throw new ArgumentNullException();
+
+            List<T> otherCopy = new();
+            foreach (var item in other)
+            {
+                otherCopy.Add(item);
+            }
+            List<T> msetCopy = new();
+            foreach (var item in dict)
+            {
+                for (int i = 0; i < item.Value; i++)
+                {
+                    msetCopy.Add(item.Key);
+                }
+            }
+
+            foreach (var item in otherCopy)
+            {
+                if (!msetCopy.Contains(item)) return false;
+                msetCopy.Remove(item);
+            }
+            return msetCopy.Count == 0;
+        }
+
+        #endregion
+
+        #region Arithmetics
+
+        public static MultiSet<T> operator +(MultiSet<T> first, MultiSet<T> second)
+        {
+            if (first is null || second is null) throw new ArgumentNullException();
+
+            MultiSet<T> result = new();
+            foreach (var item in first)
+                result.Add(item);
+            foreach (var item in second)
+                result.Add(item);
+
+            return result;
+        }
+        public static MultiSet<T> operator -(MultiSet<T> first, MultiSet<T> second)
+        {
+            if (first is null || second is null) throw new ArgumentNullException();
+
+            MultiSet<T> result = new();
+            foreach (var item in first)
+                result.Add(item);
+            foreach (var item in second)
+                if (result.Contains(item)) result.Remove(item);
+
+            return result;
+        }
+        public static MultiSet<T> operator *(MultiSet<T> first, MultiSet<T> second)
+        {
+            if (first is null || second is null) throw new ArgumentNullException();
+
+            MultiSet<T> secondCopy = new();
+            foreach (var item in second)
+                secondCopy.Add(item);
+
+            MultiSet<T> result = new();
+            foreach (var item in first)
+            {
+                if (secondCopy.Contains(item))
+                {
+                    result.Add(item);
+                    secondCopy.Remove(item);
+                }
+            }
+
+            return result;
+        }
+
+
         #endregion
 
         #region Static Methods
@@ -177,6 +405,7 @@ namespace MultiSet
         {
             get => new();
         }
+
         #endregion
 
         #region Constructors
@@ -184,6 +413,28 @@ namespace MultiSet
         {
             dict = new();
         }
+        public MultiSet(IEnumerable<T> sequence)
+        {
+            dict = new();
+            foreach (var item in sequence)
+            {
+                Add(item);
+            }
+        }
+        public MultiSet(IEqualityComparer<T> comparer) 
+        {
+            dict = new(comparer);
+            
+        }
+        public MultiSet(IEnumerable<T> sequence, IEqualityComparer<T> comparer)
+        {
+            dict = new(comparer);
+            foreach (var item in sequence)
+            {
+                Add(item);
+            }
+        }
+
         #endregion
     }
 }
